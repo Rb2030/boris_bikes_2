c36a1c2ad400134aa2baf19b7e7764b35cbc8aa1
Rb2030 <rossmail2050@gmail.com>
2016-09-06 18:48:11 +0100

    Complex Attributes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,41 +1,46 @@
 require 'docking_station'
 
 describe DockingStation do
+  it { is_expected.to respond_to :release_bike }
+  it { is_expected.to respond_to(:dock).with(1).argument }
+  it {is_expected.to respond_to(:bike) }
 
   describe '#release_bike' do
+
     it 'releases a bike' do
       bike = Bike.new
       subject.dock(bike)
-      expect(subject.release_bike). to eq bike
+      expect(bike).to be_working
+      expect(subject.release_bike).to eq bike
+    end
+
+    it 'raises an error when empty' do
+      expect { subject.release_bike }. to raise_error 'No bikes available'
     end
   end
 
-  describe '#release_bike' do
-    it 'raises an error when there are no bikes available' do
-    expect { subject.release_bike }. to raise_error 'No bikes available'
+  describe 'working?' do
+    it 'releases working bikes' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(bike).to be_working
+      subject.release_bike
+      # bike = subject.release_bike
+    end
   end
 
-  #  it { is_expected.to respond_to :release_bike }
-   #
-  #  it 'releases working bikes' do
-  #    bike = subject.release_bike
-  #    expect(bike).to be_working
-  #
-  #  end
+  describe '#dock' do
 
-   it { is_expected.to respond_to(:dock).with(1).argument }
+    it 'returns docked bikes' do
+        bike = Bike.new
+        subject.dock(bike)
+        expect(bike).to be_working
+        expect(subject.dock(bike).last).to eq bike
+    end
 
-   it {is_expected.to respond_to(:bike) }
-
-   it 'docks something' do
-     bike = Bike.new
-     expect(subject.dock(bike)).to eq bike
-   end
-
-   it 'returns docked bikes' do
-     bike = Bike.new
-     subject.dock(bike)
-     expect(subject.bike).to eq bike
-   end
+    it 'raises an error when the docking station is full' do
+        20.times { subject.dock Bike.new }
+        expect { subject.dock Bike.new }. to raise_error 'Docking station full'
+    end
   end
- end
+end
